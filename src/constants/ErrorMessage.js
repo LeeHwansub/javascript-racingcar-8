4b77975497189e8828b0e34b5ba27b1774c5d7af
USER <dyfrkfl1@gmail.com>
2025-10-27 22:13:43 +0900

    feat: 에러 메시지 세분화 및 중앙 관리
    - 자동차 이름 관련 에러 메시지 4가지로 세분화
    - 이동 횟수 관련 에러 메시지 6가지로 세분화
    - 구체적인 에러 원인 파악 가능하도록 개선
    - 사용자에게 명확한 피드백 제공

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -1,10 +1,18 @@
 // 에러 메시지 관리
 export class ErrorMessage {
+  // 자동차 이름 관련 에러
   static EMPTY_CAR_NAMES = "[ERROR] 자동차 이름을 입력해주세요.";
   static INVALID_CAR_NAME_LENGTH = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+  static INVALID_CAR_NAME_SPACE = "[ERROR] 자동차 이름에 공백이 포함될 수 없습니다.";
+  static EMPTY_CAR_NAME_IN_LIST = "[ERROR] 빈 자동차 이름이 포함되어 있습니다.";
+  
+  // 이동 횟수 관련 에러
   static EMPTY_MOVEMENT_COUNT = "[ERROR] 시도할 횟수를 입력해주세요.";
-  static INVALID_MOVEMENT_COUNT = "[ERROR] 시도 횟수는 1 이상의 정수여야 합니다.";
   static INVALID_NUMBER_FORMAT = "[ERROR] 숫자만 입력해주세요.";
+  static INVALID_MOVEMENT_COUNT_NEGATIVE = "[ERROR] 시도 횟수는 1 이상이어야 합니다.";
+  static INVALID_MOVEMENT_COUNT_ZERO = "[ERROR] 시도 횟수는 0보다 커야 합니다.";
+  static INVALID_MOVEMENT_COUNT_DECIMAL = "[ERROR] 시도 횟수는 정수여야 합니다.";
+  static INVALID_MOVEMENT_COUNT_SPACE = "[ERROR] 시도 횟수에 공백이 포함될 수 없습니다.";
 }
 
 
