ac0779f44789428e16d82357b8518928788bcb85
USER <dyfrkfl1@gmail.com>
2025-10-27 20:24:04 +0900

    feat: 상수 및 에러 메시지 분리
    - constants.js: 게임 설정 상수 정의
    - ErrorMessage.js: 에러 메시지 중앙 관리

---
+++ src/constants/ErrorMessage.js
@@ -0,0 +1,9 @@
+// 에러 메시지 관리
+export class ErrorMessage {
+  static EMPTY_CAR_NAMES = "[ERROR] 자동차 이름을 입력해주세요.";
+  static INVALID_CAR_NAME_LENGTH = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+  static EMPTY_MOVEMENT_COUNT = "[ERROR] 시도할 횟수를 입력해주세요.";
+  static INVALID_MOVEMENT_COUNT = "[ERROR] 시도 횟수는 1 이상의 정수여야 합니다.";
+  static INVALID_NUMBER_FORMAT = "[ERROR] 숫자만 입력해주세요.";
+}
+
---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+// 상수 정의
+export const MIN_ADVANCE_NUMBER = 4;
+export const MAX_ADVANCE_NUMBER = 9;
+export const MIN_MOVEMENT_COUNT = 1;
+export const MAX_CAR_NAME_LENGTH = 5;
+export const SEPARATOR = ",";
+
