86efd732f813db8c9c5663ec648e1ca184fa3abe
USER <dyfrkfl1@gmail.com>
2025-10-27 20:25:06 +0900

    refactor: App 클래스 간소화
    - Controller에 전체 로직 위임하여 App은 진입점 역할만 수행
    - Clean Code 원칙에 따라 단순하고 명확한 구조로 개선

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { GameController } from "./controllers/GameController.js";
+
+/**
+ * 메인 애플리케이션
+ * 애플리케이션 진입점 역할
+ */
 class App {
-  async run() {}
+  async run() {
+    const controller = new GameController();
+    await controller.run();
+  }
 }
 
 export default App;
